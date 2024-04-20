0a6796a0db12f9287c6ec89798b71f64dd0735de
AptekarM <161214747+AptekarM@users.noreply.github.com>
2024-04-20 21:59:28 +0400

    Внесены изменения

---
+++ MovieQuiz-ios/MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,18 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Maksim  on 26.03.2024.
+//
+
+import Foundation
+
+// Структура для представления вопроса в квизе
+struct QuizQuestion {
+    // Строка с названием фильма, совпадает с названием картинки афиши фильма в Assets
+    let image: Data
+    // Строка с вопросом о рейтинге фильма
+    let text: String
+    // Булевое значение (true, false), правильный ответ на вопрос
+    let correctAnswer: Bool
+}
---
+++ MovieQuiz-ios/MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Maksim  on 26.03.2024.
+//
+
+import Foundation
+
+protocol QuestionFactoryProtocol {
+    func requestNextQuestion()
+    func loadData()
+}
